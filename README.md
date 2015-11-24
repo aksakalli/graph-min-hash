be2b760e6a4c4379d3ac2e3e0418c51a5de99c60
Can Guney Aksakalli <cgaksakalli@gmail.com>
2015-11-24 16:53:06 +0100

    max clique hello world with jgrapht

---
+++ README.md
@@ -0,0 +1,7 @@
+# Graph Min-Hash
+
+Applying Broder's Min-Hash method to Graphs.
+
+## License
+
+Released under [the MIT license](LICENSE).
--- src/main/java/com/github/aksakalli/App.java
+++ src/main/java/com/github/aksakalli/App.java
@@ -1,10 +1,57 @@
 package com.github.aksakalli;
 
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.alg.BronKerboschCliqueFinder;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        UndirectedGraph<String, DefaultEdge> stringGraph = createStringGraph();
+
+        BronKerboschCliqueFinder  cliqueFinder = new BronKerboschCliqueFinder(stringGraph);
+        cliqueFinder.getAllMaximalCliques().stream()
+                .forEach(g -> System.out.println(g.toString()));
+
+        // note undirected edges are printed as: {<v1>,<v2>}
+        System.out.println(stringGraph.toString());
     }
+
+    /**
+     * Create a toy graph based on String objects.
+     *
+     * @return a graph based on String objects.
+     */
+    private static UndirectedGraph<String, DefaultEdge> createStringGraph()
+    {
+        UndirectedGraph<String, DefaultEdge> g =
+                new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add the vertices
+        g.addVertex(v3);
+        g.addVertex(v4);
+        g.addVertex(v1);
+        g.addVertex(v2);
+
+
+        // add edges to create a circuit
+
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v1);
+        g.addEdge(v2, v4);
+
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+        return g;
+    }
+
 }
